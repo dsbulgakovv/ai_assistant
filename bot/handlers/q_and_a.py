f3692a870f977f3077903162b2711ad53b42de8c
Булгаков Дмитрий <dmitrii.bulghakov@bk.ru>
2024-07-06 16:19:05 +0300

    Fix

--- bot/handlers/q_and_a.py
+++ bot/handlers/q_and_a.py
@@ -1,1 +1,20 @@
-router = ...
+import logging
+import os
+
+from aiogram import Bot, F, Router, types
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardRemove
+
+from keyboards.general import start_keyboard, end_keyboard
+from utils.voice_to_text_api import VoiceToTextAPI
+
+
+logger = logging.getLogger('aiogram')
+logger.setLevel(logging.DEBUG)
+
+markup_text = "<b>Текст:</b>\n{}"
+
+router = Router()
+api = VoiceToTextAPI()
