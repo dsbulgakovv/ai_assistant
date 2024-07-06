a1bf7d4a40c8504c8d8973b064cebc6af9318428
Булгаков Дмитрий <dmitrii.bulghakov@bk.ru>
2024-07-06 16:57:58 +0300

    Fix

--- bot/keyboards/general.py
+++ bot/keyboards/general.py
@@ -1,16 +1,16 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 def start_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Расшифровка голоса")
-    kb.button(text="Что-то еще")
-    kb.button(text="Хватит")
+    kb.button(text='Расшифровка голоса')
+    kb.button(text='Задать вопрос')
+    kb.button(text='Хватит')
     return kb.as_markup(resize_keyboard=True)
 
 
 def end_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Хватит")
+    kb.button(text='Хватит')
     return kb.as_markup(resize_keyboard=True)
---
+++ bot/keyboards/voice_to_text.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+# def transcript_keyboard() -> ReplyKeyboardMarkup:
+#     kb = ReplyKeyboardBuilder()
+#     kb.button(text="Хватит")
+#     return kb.as_markup(resize_keyboard=True)
