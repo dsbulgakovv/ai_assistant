74dfb00d2e451799f7a8f96b5a0f412cb976127e
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-07 23:14:28 +0300

    database edit

--- database/init_db.py
+++ database/init_db.py
@@ -16,13 +16,17 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-async def async_main(cfg):
+async def async_main():
     db_name = os.getenv('DB_NAME')
     db_user = os.getenv('DB_USER')
     db_pass = os.getenv('DB_PASS')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+    db_type = os.getenv('DB_TYPE')
+
     log.info(f"Connection to the database: '{db_name}'...")
     db_address = (
-        f"{cfg.db.type}://{db_user}:{db_pass}@{cfg.db.host}:{cfg.db.port}/{db_name}"
+        f"{db_type}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
     )
     log.info(f"Database address: '{db_address}'...")
     engine = create_async_engine(db_address, echo=True)
@@ -101,11 +105,5 @@ async def async_main(cfg):
     #     raise
 
 
-@hydra.main(config_path="configs", config_name="cfg", version_base=None)
-def main(cfg):
-    # Запускаем асинхронный код
-    asyncio.run(async_main(cfg))
-
-
 if __name__ == "__main__":
-    main()
+    asyncio.run(async_main())
