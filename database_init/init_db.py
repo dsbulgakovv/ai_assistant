7e94e3ca70841760c22af15eb5fad6792fd7cdfd
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-08 00:47:25 +0300

    database init config edit

--- database_init/init_db.py
+++ database_init/init_db.py
@@ -81,7 +81,7 @@ async def async_main(cfg):
     ]
 
     for i in range(len(filenames)):
-        asyncio.run(load_data(cfg, engine, dir_path, filenames[i], tables[i]))
+        load_data(cfg, engine, dir_path, filenames[i], tables[i])
 
 
 @hydra.main(config_path="configs", config_name="cfg", version_base=None)
