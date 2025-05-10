03e122d7e1607e1cf7251a76eb298f898be3c2c6
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-10 13:15:43 +0300

    Edit LLM api for bot

--- bot/utils/large_lang_model_api.py
+++ bot/utils/large_lang_model_api.py
@@ -1,9 +1,17 @@
+import os
+import json
 from aiohttp import ClientSession
+
+project_id = os.getenv('PROJECT_ID')
+api_key = os.getenv('KEY_ID')
+api_secret = os.getenv('KEY_SECRET')
 
 
 class LLMapi:
     def __init__(self):
-        self.base_url = "http://large_lang_model:8001"
+        self.api_completions_url = "https://api.cloud.ru/api/gigacube/openai/v1/completions"
+        self.key_id = api_key
+        self.key_secret = api_secret
 
     async def health_check(self):
         async with ClientSession() as session:
@@ -13,10 +21,45 @@ class LLMapi:
                 response = await resp.json()
         return response
 
-    async def answer(self, inp_text):
+    async def get_auth_token(self) -> str:
+        auth_url = "https://iam.api.cloud.ru/api/v1/auth/token"
+        payload = {
+            "keyId": self.key_id,
+            "secret": self.key_secret
+        }
+
         async with ClientSession() as session:
-            async with session.get(
-                f"{self.base_url}/answer_me_llm", params={"inp_text": inp_text}
+            async with session.post(
+                    auth_url,
+                    headers={"Content-Type": "application/json"},
+                    data=json.dumps(payload)
+            ) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data.get("access_token")  # Предполагаем, что токен возвращается в поле "access_token"
+                else:
+                    text = await response.text()
+                    raise Exception(f"Auth failed: {text}")
+
+    async def prompt_answer(
+        self, prompt: str, model: str, temperature: float
+    ):
+        token = await self.get_auth_token()
+        payload = {
+            "model": model,
+            "prompt": [prompt],
+            "temperature": temperature,
+            # Другие параметры по необходимости
+        }
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+        async with ClientSession() as session:
+            async with session.post(
+                f"{self.api_completions_url}",
+                headers=headers,
+                data=json.dumps(payload)
             ) as resp:
                 response = await resp.json()
         return response
