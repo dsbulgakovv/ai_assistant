b7996e5214b91922c9f7f72db47c5fff4aa75896
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-10 18:09:37 +0300

    Added handlers for task creation

--- bot/keyboards/calendar.py
+++ bot/keyboards/calendar.py
@@ -27,12 +27,13 @@ def task_name_manual_calendar_keyboard() -> ReplyKeyboardMarkup:
 
 def task_category_manual_calendar_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.row(KeyboardButton(text='Личное'))
-    kb.row(KeyboardButton(text='Работа'))
-    kb.row(KeyboardButton(text='Учеба'))
-    kb.row(KeyboardButton(text='Семья'))
-    kb.row(KeyboardButton(text='Здоровье'))
-    kb.row(KeyboardButton(text='Финансы'))
+    kb.add(KeyboardButton(text='Личное'))
+    kb.add(KeyboardButton(text='Работа'))
+    kb.add(KeyboardButton(text='Учеба'))
+    kb.row()
+    kb.add(KeyboardButton(text='Семья'))
+    kb.add(KeyboardButton(text='Здоровье'))
+    kb.add(KeyboardButton(text='Финансы'))
     kb.row(KeyboardButton(text='К предыдущему шагу'))
     kb.row(KeyboardButton(text='Отмена'))
     return kb.as_markup(resize_keyboard=True)
@@ -65,10 +66,10 @@ def task_duration_manual_calendar_keyboard() -> (InlineKeyboardMarkup, ReplyKeyb
     dur_button_4 = InlineKeyboardButton(text="2 часа", callback_data="120_min")
     dur_button_5 = InlineKeyboardButton(text="3 часа", callback_data="180_min")
     dur_button_6 = InlineKeyboardButton(text="До конца дня", callback_data="all_rest")
-    show_dur_inline_kb = InlineKeyboardMarkup(inline_keyboard=[[
-        dur_button_1, dur_button_2, dur_button_3,
-        dur_button_4, dur_button_5, dur_button_6
-    ]])
+    show_dur_inline_kb = InlineKeyboardMarkup(inline_keyboard=[
+        [dur_button_1, dur_button_2, dur_button_3],
+        [dur_button_4, dur_button_5, dur_button_6]
+    ])
 
     kb = ReplyKeyboardBuilder()
     kb.row(KeyboardButton(text='Дальше'))
