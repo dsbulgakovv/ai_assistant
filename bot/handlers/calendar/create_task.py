85bb88cdfcc9d1b70e80363d30295a36e5aa4ef9
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-10 19:59:11 +0300

    Added handlers for task creation

--- bot/handlers/calendar/create_task.py
+++ bot/handlers/calendar/create_task.py
@@ -54,7 +54,6 @@ async def create_event_task_name_manual_calendar_handler(message: types.Message,
 async def create_event_task_category_manual_calendar_handler(message: types.Message, state: FSMContext) -> None:
     if message.text.lower() == 'отмена':
         await state.set_state(StartCalendar.get_back_to_manual)
-        return
     else:
         if not message.text or message.text.strip() == "":
             await message.answer("Название не может быть пустым!")
@@ -76,7 +75,6 @@ async def create_event_task_category_manual_calendar_handler(message: types.Mess
 async def create_event_task_description_manual_calendar_handler(message: types.Message, state: FSMContext) -> None:
     if message.text.lower() == 'отмена':
         await state.set_state(StartCalendar.get_back_to_manual)
-        return
     else:
         await message.answer(
             "Введи описание события",
@@ -89,7 +87,6 @@ async def create_event_task_description_manual_calendar_handler(message: types.M
 async def create_event_task_start_manual_calendar_handler(message: types.Message, state: FSMContext) -> None:
     if message.text.lower() == 'отмена':
         await state.set_state(StartCalendar.get_back_to_manual)
-        return
     else:
         keyboards = task_start_dtm_manual_calendar_keyboard()
         await message.answer(
@@ -108,7 +105,6 @@ async def create_event_task_start_manual_calendar_handler(message: types.Message
 async def create_event_task_end_manual_calendar_handler(message: types.Message, state: FSMContext) -> None:
     if message.text.lower() == 'отмена':
         await state.set_state(StartCalendar.get_back_to_manual)
-        return
     else:
         keyboards = task_duration_manual_calendar_keyboard()
         await message.answer(
@@ -127,7 +123,6 @@ async def create_event_task_end_manual_calendar_handler(message: types.Message, 
 async def create_event_task_approval_manual_calendar_handler(message: types.Message, state: FSMContext) -> None:
     if message.text.lower() == 'отмена':
         await state.set_state(StartCalendar.get_back_to_manual)
-        return
     else:
         await message.answer(
             "ВСЕ СОБЫТИЕ",
