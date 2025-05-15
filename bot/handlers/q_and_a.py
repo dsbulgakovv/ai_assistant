d02805ebfd4a3cd9a2f20232902016bb2adcc65a
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-15 18:12:06 +0300

    Added handlers for task creation

--- bot/handlers/q_and_a.py
+++ bot/handlers/q_and_a.py
@@ -54,9 +54,10 @@ async def q_and_a_process_handler(message: types.Message, bot: Bot, state: FSMCo
                 "content": system_prompt_q_and_a.format(text['text'])
             }
         ]
-        answer = await llm_api.prompt_answer(
+        resp = await llm_api.prompt_answer(
             messages, temperature=0.1, top_p=0.1, max_tokens=3_000
-        )['choices'][0]['message']['content']
+        )
+        answer = resp['choices'][0]['message']['content']
         if len(answer) > 4080:
             for x in range(0, len(answer), 4080):
                 await message.answer(markup_text.format(answer)[x:x + 4095], reply_markup=end_keyboard())
@@ -73,9 +74,10 @@ async def q_and_a_process_handler(message: types.Message, bot: Bot, state: FSMCo
                 "content": system_prompt_q_and_a.format(message.text)
             }
         ]
-        answer = await llm_api.prompt_answer(
+        resp = await llm_api.prompt_answer(
             messages, temperature=0.1, top_p=0.1, max_tokens=3_000
-        )['choices'][0]['message']['content']
+        )
+        answer = resp['choices'][0]['message']['content']
         if len(answer) > 4080:
             for x in range(0, len(answer), 4080):
                 await message.answer(markup_text.format(answer)[x:x + 4095], reply_markup=end_keyboard())
