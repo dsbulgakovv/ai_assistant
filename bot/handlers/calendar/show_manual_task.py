bec08c51be52521af97da147bfb6832450522861
tic-tac-ml <dmitrii.bulghakov@bk.ru>
2025-05-18 19:54:01 +0300

    Added handlers for task edition

--- bot/handlers/calendar/show_manual_task.py
+++ bot/handlers/calendar/show_manual_task.py
@@ -442,8 +442,8 @@ async def editing_task_start_event_start(message: types.Message, state: FSMConte
         user_timezone = data['user_timezone']
         event = data['events'][data['editing_event_num'] - 1]
         new_event_info = event.copy()
-        new_event_info['start_dtm'] = selected_datetime
-        new_event_info['end_dtm'] = after_selected_datetime
+        new_event_info['task_start_dtm'] = selected_datetime
+        new_event_info['task_end_dtm'] = after_selected_datetime
         new_text = form_one_event_detailed(new_event_info, user_timezone)
         await state.update_data(new_event_info=new_event_info)
         msg = await message.answer(new_text, reply_markup=editing_approve_task())
